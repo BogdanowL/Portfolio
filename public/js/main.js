453ee4b63fee9e988ec7893db6b2c194f5a0ae78
BogdanowL <62738964+BogdanowL@users.noreply.github.com>
2021-02-03 13:53:41 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,10 @@ $(function () {
 
         $('#btn-rand').click(function () {
             let rand = Math.floor(Math.random() * 25) + 1;
-            open(`http://lovelywish/profile/${rand}`)
+            open(`http://bogdanovmaster.site/profile/${rand}`)
         });
         $('#btn-reg').click(function () {
-            open('http://lovelywish/register')
+            open('http://bogdanovmaster.site/register')
         });
         $('#btn-hh').click(function () {
             open('https://hh.ru/applicant/resumes/view?resume=a0aa1c29ff07e2cd9b0039ed1f535375427a47')
